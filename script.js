cc604d3e3d1b3cdecc9e033667041e5b056bfc2d
Nicolás Santísima Trinidad Santín <sannicolas2121@outlook.com>
2025-01-21 16:41:00 +0100

    Improve the GUI

--- script.js
+++ script.js
@@ -2,22 +2,43 @@ const $ = selector => document.querySelector(selector)
 
 const search = $('input')
 
+let imgRoute = ''
+
 search.addEventListener('input', () => {
-    const value = search.value
+    const value = search.value.toLowerCase()
     fetch(`https://pokeapi.co/api/v2/pokemon/${value}`)
         .then(res => res.json())
         .then(response => {
             search.classList.remove('error')
             search.classList.add('sucess')
-            $('h1').textContent = response['name']
-            $('.show').src = response['sprites']['front_default']
+            $('#name').textContent = response['name'].toUpperCase()
+            imgRoute = response['sprites']['front_default']
+            $('.show').src = imgRoute
             return
         })
         .catch(() => {
             search.classList.remove('sucess')
             search.classList.add('error')
-            $('h1').textContent = ''
-            $('.show').src = ''
+            $('#name').textContent = ''
+            imgRoute = ''
+            $('.show').src = imgRoute
             return
         })
+})
+
+const help = $('button')
+
+help.addEventListener('click', () => {
+    if ($('#helpDiv').classList.contains('noDisplay')) {
+        $('#helpDiv').classList.remove('noDisplay')
+        $('#helpDiv').classList.add('helpDiv')
+    }
+    else {
+        $('#helpDiv').classList.add('noDisplay')
+        $('#helpDiv').classList.remove('helpDiv')
+    }
+})
+
+$('.show').addEventListener('click', () => {
+    alert(imgRoute)
 })
